bfac5c012f51337783b73a82b2107aaf2aed2a20
axelr <axelrepeat@gmail.com>
2025-09-25 19:42:03 -0600

    Se agregó el archivo dimens.xml para manejar el atributo padding de los elementos de la pantalla y se completo el elemento componible TopicCard, listo para usarlo en listas

--- app/src/main/java/com/example/courses/MainActivity.kt
+++ app/src/main/java/com/example/courses/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.courses.model.Topic
 import com.example.courses.ui.theme.CoursesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +34,62 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CoursesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+
+            }
+        }
+    }
+}
+
+@Composable
+fun TopicApp() {
+
+}
+
+@Composable
+fun TopicCard(
+    topic: Topic,
+    modifier: Modifier = Modifier
+) {
+    Card (modifier = modifier) {
+        Row {
+            Image(
+                painter = painterResource(topic.imageRes),
+                contentDescription = stringResource(topic.name),
+                modifier = Modifier
+                    .height(68.dp)
+                    .width(68.dp)
+                    .padding(end = dimensionResource(R.dimen.padding_medium)),
+                contentScale = ContentScale.Crop
+            )
+            Column (
+                modifier = modifier
+                    .padding(
+                        0.dp,
+                        dimensionResource(R.dimen.padding_medium),
+                        dimensionResource(R.dimen.padding_medium),
+                    ),
+                verticalArrangement = Arrangement.Center
+            ){
+                Text(
+                    text = stringResource(topic.name),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier
+                        .padding(bottom = dimensionResource(R.dimen.padding_small))
+                )
+                Row (
+                    modifier = Modifier,
+                    //horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier
+                    )
+
+                    Text(
+                        text = topic.availableCourses.toString(),
+                        style = MaterialTheme.typography.labelMedium
                     )
                 }
             }
@@ -30,18 +97,14 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TopicCardPreview() {
     CoursesTheme {
-        Greeting("Android")
+        TopicCard(
+            Topic(R.string.architecture, 15, R.drawable.crafts),
+            modifier = Modifier
+        )
     }
+
 }
