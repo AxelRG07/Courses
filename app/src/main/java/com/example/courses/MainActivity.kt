ddb3e65a238bcfb63818b40b5cb01cb2e0199efc
axelr <axelrepeat@gmail.com>
2025-09-25 20:17:18 -0600

    Se implementó un elemento LazyVerticalGrid para mostrar la lista de cursos adecuadamente, todo esto dentro del elemento componible TopicList que luego es ajustado a toda la pantalla en la función TopicApp.

--- app/src/main/java/com/example/courses/MainActivity.kt
+++ app/src/main/java/com/example/courses/MainActivity.kt
@@ -6,15 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -25,6 +31,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.courses.data.DataSource
 import com.example.courses.model.Topic
 import com.example.courses.ui.theme.CoursesTheme
 
@@ -34,7 +41,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CoursesTheme {
-
+                TopicApp()
             }
         }
     }
@@ -42,7 +49,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TopicApp() {
-
+    Surface (
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .padding(bottom = dimensionResource(R.dimen.padding_small)),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        TopicList(
+            topicList = DataSource.topics,
+            modifier = Modifier.padding(
+                start = dimensionResource(R.dimen.padding_small),
+                top = dimensionResource(R.dimen.padding_small),
+                end = dimensionResource(R.dimen.padding_small),
+            )
+        )
+    }
 }
 
 @Composable
@@ -52,15 +74,17 @@ fun TopicCard(
 ) {
     Card (modifier = modifier) {
         Row {
-            Image(
-                painter = painterResource(topic.imageRes),
-                contentDescription = stringResource(topic.name),
-                modifier = Modifier
-                    .height(68.dp)
-                    .width(68.dp)
-                    .padding(end = dimensionResource(R.dimen.padding_medium)),
-                contentScale = ContentScale.Crop
-            )
+            Box {
+                Image(
+                    painter = painterResource(topic.imageRes),
+                    contentDescription = stringResource(topic.name),
+                    modifier = Modifier
+                        .height(68.dp)
+                        .width(68.dp)
+                        .padding(end = dimensionResource(R.dimen.padding_medium)),
+                    contentScale = ContentScale.Crop
+                )
+            }
             Column (
                 modifier = modifier
                     .padding(
@@ -97,13 +121,26 @@ fun TopicCard(
     }
 }
 
+@Composable
+fun TopicList(topicList: List<Topic>, modifier: Modifier = Modifier){
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+        modifier = modifier
+    ){
+        items(topicList) { topic ->
+            TopicCard(topic)
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun TopicCardPreview() {
     CoursesTheme {
-        TopicCard(
-            Topic(R.string.architecture, 15, R.drawable.crafts),
-            modifier = Modifier
+        TopicList(
+            topicList = DataSource.topics
         )
     }
 
